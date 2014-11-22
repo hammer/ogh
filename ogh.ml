c95786eb1783f57a7f9673e7bbdb7d08a57573d8
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-11-22 17:44:02 -0500

    Got the preamble working with an already-obtained token. Time to start doing some work.

--- ogh.ml
+++ ogh.ml
@@ -1,9 +1,12 @@
+let gh_user = Sys.getenv "GITHUB_USER"
+let gh_pass = Sys.getenv "GITHUB_PASSWORD"
+
+(* This code assumes you have already gotten the token
+   and saved it locally in your cookie jar *)
 let t =
-  let gh_user = Sys.getenv "GITHUB_USER" in
-  let gh_pass = Sys.getenv "GITHUB_PASSWORD" in
-  let r = Github.Token.create ~user:gh_user ~pass:gh_pass ~note:"get_token via ocaml-github" () in
-  lwt auth = Github.Monad.run r in
-  let token = Github.Token.of_auth auth in
+  lwt ghcj = Github_cookie_jar.init () in
+  lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
+  let token = Github.Token.of_auth (BatOption.get auth) in
   prerr_endline (Github.Token.to_string token);
   Lwt.return ()
 
