b2ca911fc8c0fd2fed27f23c0c8a792f170a33ce
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-11-23 02:56:34 -0500

    One way to make multiple RPCs. Certainly not the best way.

--- ogh.ml
+++ ogh.ml
@@ -8,17 +8,26 @@ let gh_pass = Sys.getenv "GITHUB_PASSWORD"
 (* This code assumes you have already gotten the token
    and saved it locally in your cookie jar *)
 let t =
+  lwt ghcj = Github_cookie_jar.init () in
+  lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
+  let token = Github.Token.of_auth (BatOption.get auth) in
   lwt r =
-    lwt ghcj = Github_cookie_jar.init () in
-    lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
-    let token = Github.Token.of_auth (BatOption.get auth) in
     let open Github.Monad in
     run (
     Github.Repo.info ~token ~user:"hammer" ~repo:"ogh" () >>=
     fun info ->
       eprintf "Description of ogh repo: %s\n" info.repo_description;
       return ()
-      ) in
+    ) in
+  return ();
+  lwt q =
+    let open Github.Monad in
+    run (
+    Github.Repo.info ~token ~user:"hammer" ~repo:"ocass" () >>=
+    fun info ->
+      eprintf "Description of ocass repo: %s\n" info.repo_description;
+      return ()
+    ) in
   return ()
 
 let _ = Lwt_main.run t
