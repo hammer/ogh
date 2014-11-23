538bd374b2339b8211cde410d685e4b73fa928ba
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-11-23 02:44:14 -0500

    Retrieving information about a repo successfully.

--- ogh.ml
+++ ogh.ml
@@ -4,10 +4,17 @@ let gh_pass = Sys.getenv "GITHUB_PASSWORD"
 (* This code assumes you have already gotten the token
    and saved it locally in your cookie jar *)
 let t =
-  lwt ghcj = Github_cookie_jar.init () in
-  lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
-  let token = Github.Token.of_auth (BatOption.get auth) in
-  prerr_endline (Github.Token.to_string token);
+  lwt r =
+    lwt ghcj = Github_cookie_jar.init () in
+    lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
+    let token = Github.Token.of_auth (BatOption.get auth) in
+    let open Github.Monad in
+    run (
+    Github.Repo.info ~token ~user:"hammer" ~repo:"ogh" () >>=
+    fun info ->
+      Printf.eprintf "repo %s\n" info.repo_description;
+      return ()
+      ) in
   Lwt.return ()
 
 let _ = Lwt_main.run t
