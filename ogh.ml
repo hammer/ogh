d0a8065639e08dff4c069c531f7df97f621c0022
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-11-24 23:43:56 -0500

    Borrowing some helper functions from avsm.

--- ogh.ml
+++ ogh.ml
@@ -5,29 +5,24 @@ open Github_t
 let gh_user = Sys.getenv "GITHUB_USER"
 let gh_pass = Sys.getenv "GITHUB_PASSWORD"
 
+let ask_github fn = Github.(Monad.run (fn ()))
+
+let print_repo_info m =
+  let open Github_t in
+  eprintf "repo %s: %s (%d watchers, %d open issues)\n%!"
+          m.repo_name m.repo_description m.repo_watchers m.repo_open_issues
+
+let get_token cookie_name =
+  lwt ghcj = Github_cookie_jar.init () in
+  lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
+  return (Github.Token.of_auth (BatOption.get auth))
+
 (* This code assumes you have already gotten the token
    and saved it locally in your cookie jar *)
 let t =
-  lwt ghcj = Github_cookie_jar.init () in
-  lwt auth = Github_cookie_jar.get ghcj ~name:"ogh" in
-  let token = Github.Token.of_auth (BatOption.get auth) in
-  lwt r =
-    let open Github.Monad in
-    run (
-    Github.Repo.info ~token ~user:"hammer" ~repo:"ogh" () >>=
-    fun info ->
-      eprintf "Description of ogh repo: %s\n" info.repo_description;
-      return ()
-    ) in
-  return ();
-  lwt q =
-    let open Github.Monad in
-    run (
-    Github.Repo.info ~token ~user:"hammer" ~repo:"ocass" () >>=
-    fun info ->
-      eprintf "Description of ocass repo: %s\n" info.repo_description;
-      return ()
-    ) in
+  get_token "ogh" >>= fun token ->
+  ask_github (Github.Repo.info ~token ~user:"hammer" ~repo:"ogh") >|= print_repo_info >>
+  ask_github (Github.Repo.info ~token ~user:"hammer" ~repo:"ocass") >|= print_repo_info >>
   return ()
 
 let _ = Lwt_main.run t
