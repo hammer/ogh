b2bee0f839857c7460d8cd91237432246da620a7
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-11-24 23:58:11 -0500

    Use local opens

--- ogh.ml
+++ ogh.ml
@@ -1,18 +1,16 @@
-open Lwt
-open Printf
-open Github_t
-
 let gh_user = Sys.getenv "GITHUB_USER"
 let gh_pass = Sys.getenv "GITHUB_PASSWORD"
 
 let ask_github fn = Github.(Monad.run (fn ()))
 
 let print_repo_info m =
+  let open Printf in
   let open Github_t in
   eprintf "repo %s: %s (%d watchers, %d open issues)\n%!"
           m.repo_name m.repo_description m.repo_watchers m.repo_open_issues
 
 let get_token cookie_name =
+  let open Lwt in
   Github_cookie_jar.init () >>= fun ghcj ->
   Github_cookie_jar.get ghcj ~name:"ogh" >>= fun auth ->
   return (Github.Token.of_auth (BatOption.get auth))
@@ -20,6 +18,7 @@ let get_token cookie_name =
 (* This code assumes you have already gotten the token
    and saved it locally in your cookie jar *)
 let t =
+  let open Lwt in
   get_token "ogh" >>= fun token ->
   ask_github (Github.Repo.info ~token ~user:"hammer" ~repo:"ogh") >|=
   print_repo_info >>= fun () ->
