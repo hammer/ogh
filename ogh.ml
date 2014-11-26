0906a996da35903f783dd796698f5d16c947b9e5
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2014-11-25 19:57:17 -0500

    Given a cookie name and a GitHub organization, print information about all repos for the Owners team of the organization.

--- ogh.ml
+++ ogh.ml
@@ -1,13 +1,8 @@
 let gh_user = Sys.getenv "GITHUB_USER"
 let gh_pass = Sys.getenv "GITHUB_PASSWORD"
+let gh_api_base_url = "https://api.github.com"
 
 let ask_github fn = Github.(Monad.run (fn ()))
-
-let print_repo_info m =
-  let open Printf in
-  let open Github_t in
-  eprintf "repo %s: %s (%d watchers, %d open issues)\n%!"
-          m.repo_name m.repo_description m.repo_watchers m.repo_open_issues
 
 let get_token cookie_name =
   let open Lwt in
@@ -15,16 +10,52 @@ let get_token cookie_name =
   Github_cookie_jar.get ghcj ~name:"ogh" >>= fun auth ->
   return (Github.Token.of_auth (BatOption.get auth))
 
+let make_org_teams_uri ~org =
+  Uri.of_string (Printf.sprintf "%s/orgs/%s/teams" gh_api_base_url org)
+
+let make_team_repos_uri ~team =
+  Uri.of_string (Printf.sprintf "%s/teams/%d/repos" gh_api_base_url team)
+
+let print_repo_info ~m =
+  let open Printf in
+  let open Github_t in
+  eprintf "repo %s: %s (%d watchers, %d open issues)\n%!"
+          m.repo_name m.repo_description m.repo_watchers m.repo_open_issues
+
+let get_owners_id ~org_teams_json =
+  let open Yojson.Basic.Util in
+  let json = Yojson.Basic.from_string org_teams_json in
+  let is_owners team_el = (team_el |> member "name" |> to_string) = "Owners" in
+  let owners_team = List.filter is_owners (json |> to_list) in
+  List.hd owners_team |> member "id" |> to_int
+
+let print_repo_infos ~team_repos_json =
+  let open Yojson.Basic.Util in
+  let json = Yojson.Basic.from_string team_repos_json in
+  let gh_t_from_json repo_el = Github_j.repo_of_string (Yojson.Basic.to_string repo_el) in
+  let print_repo_from_json repo_el = print_repo_info (gh_t_from_json repo_el) in
+  List.iter print_repo_from_json  (json |> to_list)
+
+let get_org_owners_id ~token ~org =
+  let open Lwt in
+  let org_teams_uri = make_org_teams_uri org in
+  let handle_response s = return (get_owners_id s) in
+  Github.Monad.run (Github.API.get ~token:token ~uri:org_teams_uri handle_response)
+
+let get_team_repos ~token ~team =
+  let open Lwt in
+  let team_repos_uri = make_team_repos_uri team in
+  let handle_response s = return (print_repo_infos s) in
+  Github.Monad.run (Github.API.get ~token:token ~uri:team_repos_uri handle_response)
+
 (* This code assumes you have already gotten the token
    and saved it locally in your cookie jar *)
-let t =
+let print_org_repos ~cookie_name ~org =
   let open Lwt in
-  get_token "ogh" >>= fun token ->
-  ask_github (Github.Repo.info ~token ~user:"hammer" ~repo:"ogh") >|=
-  print_repo_info >>= fun () ->
-  ask_github (Github.Repo.info ~token ~user:"hammer" ~repo:"ocass") >|=
-  print_repo_info >>= fun () ->
+  get_token cookie_name >>= fun token ->
+  get_org_owners_id token org >>= fun owners_id ->
+  get_team_repos token owners_id >>= fun () ->
   return ()
 
-let _ = Lwt_main.run t
+let _ = Lwt_main.run (print_org_repos "ogh" "hammerlab")
 
